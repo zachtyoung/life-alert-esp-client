52ed7c5aa1f8bc3d700a6b3d0cefb16676703414
Zach Young <zachyoung@Zachs-iMac.local>
2021-02-01 15:59:16 -0600

    saving work

--- life-alert-esp-client.ino
+++ life-alert-esp-client.ino
@@ -1,69 +1,103 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com
-  Based on the NTP Client library example
-*********/
-
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-// Replace with your network credentials
-const char* ssid = "Zach's Wi-Fi Network";
-const char* password = "Jayhawk20";
-
-// Define NTP Client to get time
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+String weekDays[7]={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String months[12]={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-// Variables to save date and time
-String formattedDate;
-String dayStamp;
-String timeStamp;
+const char* ssid = "RobotZone";
+const char* password = "RobotZone1";
+const char* serverName = "http://192.168.1.106:1880/update-sensor";
+
+//unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+//unsigned long timerDelay = 5000;
+
+//Init button to be used for ON/OFF override swtich
+const int on_off_switch = 4;
 
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
   Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-
-// Initialize a NTPClient to get time
   timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
   timeClient.setTimeOffset(-21600);
 }
-void loop() {
-  while(!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
-  // The formattedDate comes with the following format:
-  // 2018-05-28T16:00:13Z
-  // We need to extract date and time
-  formattedDate = timeClient.getFormattedDate();
-  Serial.println(formattedDate);
 
-  // Extract date
-  int splitT = formattedDate.indexOf("T");
-  dayStamp = formattedDate.substring(0, splitT);
-  Serial.print("DATE: ");
-  Serial.println(dayStamp);
-  // Extract time
-  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
-  Serial.print("HOUR: ");
-  Serial.println(timeStamp);
-  delay(1000);
-}
+void loop() {
+   timeClient.update();
+   //Time related variables here
+   unsigned long epochTime = timeClient.getEpochTime();
+   String formattedTime = timeClient.getFormattedTime();
+   int currentHour = timeClient.getHours();
+   int currentMinute = timeClient.getMinutes();
+   int currentSecond = timeClient.getSeconds();
+   String weekDay = weekDays[timeClient.getDay()];
+   struct tm *ptm = gmtime ((time_t *)&epochTime);
+   int monthDay = ptm->tm_mday;
+   int currentMonth = ptm->tm_mon+1;
+   String currentMonthName = months[currentMonth-1];
+   int currentYear = ptm->tm_year+1900;
+   String currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
+  //Not sure which state is default -> could be LOW or HIGH
+  //If switch is ON then continue logic to record 'events'
+  if(digitalRead(on_off_switch) == HIGH){
+
+    //Logic for event goes here
+
+    
+  }
+
+
+
+
+
+
+
+
+
+  
+
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      http.begin(serverName);
+      http.addHeader("Content-Type", "application/json");      
+      int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      http.end();
+    }
+  }
+
+
+
+  /* 
+   Metrics to Track/Display 
+   DAILY
+   - Total events for the day
+   WEEKLY
+   - Total events for the week
+   - Day with most events
+
+   MONTHLY
+   -Total events for the month
+   -Day with the most events
+   -Week with the most events
+
+
+
+  
+  */
